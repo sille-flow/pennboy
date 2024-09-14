da4dcb43f92f92e7b7d2a6bcd6cad7e31278aa92
DesmosReborn <makev@seas.upenn.edu>
2024-09-14 16:50:09 -0400

    player script variable initialization

--- Assets/HMTT/Scripts/PlayerScript.cs
+++ Assets/HMTT/Scripts/PlayerScript.cs
@@ -4,10 +4,32 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private float maxSpeed;
+    [SerializeField]
+    private float jumpForce;
+    [SerializeField]
+    private float doubleJumpForce;
+    private bool canDoubleJump;
+    [SerializeField]
+    private float gravityForce;
+    [SerializeField]
+    private float dashSpeed;
+    private bool canDash;
+    private Collider col;
+    [SerializeField]
+    private float wallJumpForce;
+    private bool isWallJumping;
+    private bool isAlive;
+    private bool isActive;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        col = GetComponent<Collider>();
     }
 
     // Update is called once per frame
