8a3124789839522a565f260639e98b7927b74f7d
DesmosReborn <makev@seas.upenn.edu>
2024-09-14 22:20:23 -0400

    added player variables, created getters and setters, minor base scene setup

--- Assets/HMTT/Scripts/PlayerScript.cs
+++ Assets/HMTT/Scripts/PlayerScript.cs
@@ -5,31 +5,45 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     [SerializeField]
-    private float moveSpeed;
+    private float moveSpeed { get; set; }
     [SerializeField]
-    private float maxSpeed;
+    private float maxSpeed { get; set; }
     [SerializeField]
-    private float jumpForce;
+    private float jumpForce { get; set; }
     [SerializeField]
-    private float doubleJumpForce;
+    private float doubleJumpForce { get; set; }
     private bool canDoubleJump;
     [SerializeField]
-    private float gravityForce;
+    private float gravityForce { get; set; }
     [SerializeField]
-    private float dashSpeed;
-    private bool canDash;
+    private float dashSpeed { get; set; }
+    private bool isDashing { get; }
+    private bool canDash { get; set; }
+    private float dashCD { get; set; }
     private Collider col;
+    private Rigidbody rb;
     [SerializeField]
-    private float wallJumpForce;
+    private float wallJumpForce { get; set; }
+    private bool isOnWall { get; }
     private bool isWallJumping;
-    private bool isAlive;
-    private bool isActive;
+    private bool isAlive { get; set; }
+    private bool isActive { get; set; }
 
+    public void addForce(Vector3 force)
+    {
+        rb.AddForce(force);
+    }
+
+    public Vector3 getVelocity()
+    {
+        return rb.velocity;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         col = GetComponent<Collider>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
