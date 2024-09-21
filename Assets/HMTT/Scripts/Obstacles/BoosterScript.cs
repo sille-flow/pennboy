170bc5663eb77bd4c4e741e75145ea15aa865890
qihebing <qihecui2023@gmail.com>
2024-09-21 14:27:14 -0400

    completed booster script

--- Assets/HMTT/Scripts/Obstacles/BoosterScript.cs
+++ Assets/HMTT/Scripts/Obstacles/BoosterScript.cs
@@ -8,11 +8,13 @@ public class BoosterScript : MonoBehaviour
     //of this script is to give the gameobject the ability to propel the player upwards upon contact.
 
     //declare variables here
+    public float launchForce = 10.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         //The start function is called when the object is created, initialize variables here
+        launchForce = 10.0f;
     }
 
     // Update is called once per frame
@@ -28,10 +30,13 @@ public class BoosterScript : MonoBehaviour
         if (other.gameObject.tag == "Player") //This line checks to see that the collided object is the Player
         {
             //The code that controls what happens when the player steps on the booster goes here
+            Rigidbody playerRb = other.GetComponent<Rigidbody>();
+            playerRb.velocity = new Vector3(playerRb.velocity.x, 0, playerRb.velocity.z);
+            playerRb.AddForce(Vector3.up * launchForce, ForceMode.Impulse); 
         }
     }
-
     //Once you're finished writing this script, the actual gameobject must be made. Create the piece of geometry
     //you envision this booster to take and drag this script into it. Add a collider and edit it to the correct size.
     //Then, drag the gameobject into the prefabs folder to turn it into a prefab.
 }
+            //playerRb.velocity(Vector3.up * 0);
