acfcf642ce4295acacfa5274a8c9a67be6522f5a
exaCORE42 <exacore42@git>
2024-09-20 23:28:53 -0400

    level prefab

--- Assets/Group 6/Scripts/Finish_Line.cs
+++ Assets/Group 6/Scripts/Finish_Line.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class Finish_Line : MonoBehaviour
 {
+    [SerializeField]
+    Game level;
     // Start is called before the first frame update
     void Start()
     {
