12b33da2cbb64e15d5dbe7c84109d09ee4100b64
exaCORE42 <40a15b8f-bd19-4c61-844c-45c0c4aab94f@aleeas.com>
2024-09-28 16:43:16 -0400

    fix bug

--- Assets/Group 6/Scripts/Game.cs
+++ Assets/Group 6/Scripts/Game.cs
@@ -22,7 +22,7 @@ public class Game : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (budget == 0) {
+        if (budget <= 0) {
             SceneManager.LoadScene(loseScene);
         }
     }
