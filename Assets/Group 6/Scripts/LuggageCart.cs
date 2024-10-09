e7101f09a26a211c93fc4d7ad874981a4e698141
exaCORE42 <40a15b8f-bd19-4c61-844c-45c0c4aab94f@aleeas.com>
2024-10-09 19:13:33 -0400

    replace all walls with wall prefab, delete wall tag

--- Assets/Group 6/Scripts/LuggageCart.cs
+++ Assets/Group 6/Scripts/LuggageCart.cs
@@ -10,12 +10,6 @@ public class LuggageCart : MonoBehaviour
     [SerializeField] Game level;
     public float maxSpeed = 15f;
     private float maxSqrSpeed;
-    [SerializeField] private float damageSlope = 1;
-    [SerializeField] private float damageOffset = 0;
-    [SerializeField] private float maxDamage = 100;
-    [SerializeField] private float cooldownTime;
-    [SerializeField] private AudioSource crashSound;
-    private float currCooldownTime = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,9 +19,6 @@ public class LuggageCart : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        if (currCooldownTime > 0) {
-            currCooldownTime = Mathf.Clamp(currCooldownTime - Time.deltaTime, 0, cooldownTime);
-        }
         if(GetComponent<Rigidbody>().velocity.sqrMagnitude > maxSqrSpeed)
         {
             GetComponent<Rigidbody>().velocity = GetComponent<Rigidbody>().velocity.normalized * maxSpeed;
@@ -35,19 +26,9 @@ public class LuggageCart : MonoBehaviour
     }
 
     void OnCollisionEnter(Collision collision) {
-        int damage;
         PropertyDamageCollider col = collision.gameObject.GetComponent<PropertyDamageCollider>();
-        Debug.Log(collision.gameObject.name);
-        if (collision.gameObject.tag == "Group6Wall") {
-            crashSound.Play(0);
-            damage = Mathf.RoundToInt(Mathf.Clamp(damageSlope * collision.impulse.magnitude + damageOffset, 0, maxDamage));
-            Debug.Log(damage);
-            if (damage > 0 && currCooldownTime == 0) {
-                currCooldownTime = cooldownTime;
-                level.reduceBudget(damage);
-            }
-        } else if (col != null) {
-            damage = col.calculateDamage(collision.impulse.magnitude);
+        if (col != null) {
+            int damage = col.calculateDamage(collision.impulse.magnitude);
             if (damage != 0) {
                 level.reduceBudget(damage);
             }
