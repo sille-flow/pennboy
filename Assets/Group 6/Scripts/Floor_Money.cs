9fd017035a1c76df445ada0b1a3bb120c63aec5e
Forest Ho-Chen <foresth168@gmail.com>
2024-11-09 14:23:33 -0500

    Money

---
+++ Assets/Group 6/Scripts/Floor_Money.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor_Money : MonoBehaviour
+{
+    [SerializeField] Game level;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 1, 0);
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        PropertyDamageCollider col = collision.gameObject.GetComponent<PropertyDamageCollider>();
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.name == "Player") {
+            Debug.Log("Collide with player");
+            level.reduceBudget(-1000);
+            Destroy(gameObject);
+        }
+    }
+}
