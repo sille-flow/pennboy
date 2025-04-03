9cd487a1eda14b0e2b9b989a87bb252e57bc82ff
leebwj <109943383+leebwj@users.noreply.github.com>
2025-04-02 20:55:48 -0400

    Door Animation
    Door Animation

---
+++ Assets/Game/Scripts/DoorAnimation.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimation : MonoBehaviour
+{
+
+    private Animator _doorAnimator;
+    void Start()
+    {
+        _doorAnimator = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _doorAnimator.SetTrigger("Open");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _doorAnimator.SetTrigger("Closed");
+        }
+    }
+}
