7f376e36462eab49d9ebcf8bb9d0d3b7d50c3318
leebwj <109943383+leebwj@users.noreply.github.com>
2025-04-26 15:55:07 -0400

    door scripts

---
+++ Assets/Game/Scripts/DoorAnimationTwo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DoorAnimationTwo : MonoBehaviour
+{
+    private Animator _doorAnimator;
+
+    void Start()
+    {
+        _doorAnimator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        // Get the player's inventory
+        Inventory playerInventory = other.GetComponent<Inventory>();
+        if (playerInventory != null && playerInventory.NumberOfCollectibles > 3)
+        {
+            _doorAnimator.SetTrigger("Open");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        // Only close if the door was open (i.e. they still have ≥4)
+        Inventory playerInventory = other.GetComponent<Inventory>();
+        if (playerInventory != null && playerInventory.NumberOfCollectibles > 3)
+        {
+            _doorAnimator.SetTrigger("Closed");
+        }
+    }
+}
